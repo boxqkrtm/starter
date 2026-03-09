012a5e7789565de665fd9e23ff1d11fa778698bc
boxqkrtm <9902abcd@gmail.com>
2026-03-10 00:25:29 +0900

    feat: add cursortab.nvim config for local FIM backend

---
+++ lua/plugins/minuet.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "leonardcser/cursortab.nvim",
+    build = "cd server && go build",
+    lazy = false,
+    config = function()
+      require("cursortab").setup({
+        provider = {
+          type = "fim",
+          url = "http://localhost:11434",
+          completion_path = "/v1/completions",
+          model = "sweepai/sweep-next-edit",
+        },
+      })
+    end,
+  },
+}
