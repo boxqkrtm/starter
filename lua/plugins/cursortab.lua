4eaa79c0d88a6990f5de383c8634f5cffa54f3f7
boxqkrtm <9902abcd@gmail.com>
2026-03-18 11:21:51 +0900

    chore: update lock, edit cursortab setting

--- lua/plugins/cursortab.lua
+++ lua/plugins/cursortab.lua
@@ -1,15 +1,24 @@
 return {
   {
     "leonardcser/cursortab.nvim",
-    build = "cd server && go build",
     lazy = false,
+    build = "cd server && go build",
     config = function()
       require("cursortab").setup({
+        behavior = {
+          idle_completion_delay = 500,
+          text_change_debounce = 500,
+          cursor_prediction = {
+            enabled = true,
+            auto_advance = true,
+            proximity_threshold = 2,
+          },
+        },
         provider = {
           type = "sweep",
           url = "http://localhost:11434",
-          completion_path = "/v1/completions",
-          model = "sweepai/sweep-next-edit",
+          model = "sweepai/sweep-next-edit:latest",
+          completion_timeout = 20000,
         },
       })
     end,
